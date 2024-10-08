5f66026110b58858cc4f737ed94da2eceba6c83f
IsmatQasim <ismat6812@gmail.com>
2024-10-08 22:57:02 +0500

    Added Fonts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Button } from '@mui/material';
-import "./index.css"
+import "./index.css";
 
 function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-200">
       <div className="bg-white shadow-lg rounded-lg p-6">
-        <h1 className="text-2xl font-bold mb-4">Hello, World!</h1>
+        <h1 className="text-2xl font-bold mb-4 bold">Hello, World!</h1>
         <Button variant="contained" color="primary">
           MUI Button
         </Button>
