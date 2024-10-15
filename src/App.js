a45e43de30c3a3ac1b2e1dcea1371804d2af9af0
IsmatQasim <ismat6812@gmail.com>
2024-10-15 21:16:55 +0500

    Created Pages Of Stories

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 //import "./index.css";
 // import CommunityAndPrivacy from "./Pages/Community&Privacy";
 //import Profile from "./Pages/Profile";
-import './Styles/CreateStoryForm.css'; // Import the CSS for styling
-import StoryCreatePage from './Pages/CreateYourOwnStory'; // Adjust the import for your folder structure
+import './Styles/CreateStoryForm.css'; 
+import Stories from "./Pages/Stories";
+// import StoryCreatePage from './Pages/CreateYourOwnStory'; 
 
 function App() {
   return (
     <div>
-      <StoryCreatePage /> {/* Render the StoryCreatePage component */}
+    <Stories />
+      {/* <StoryCreatePage />  */}
        {/* <CommunityAndPrivacy /> */}
        {/*<Profile />*/ }
+
       
     </div>
   );
---
+++ src/Pages/Stories.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../Styles/Stories.css"
+
+const Stories = () => {
+  return (
+    <div className='storyBackground'>
+
+
+    </div>
+  )
+}
+
+export default Stories
